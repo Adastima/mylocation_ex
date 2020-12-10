7881fef8fa3f55e03209dacb917f032bb9432abc
Adika <adastima@gmail.com>
2020-12-10 10:23:44 +0800

    newest

--- mylocation/ViewController.swift
+++ mylocation/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet var  latLabel: UILabel!
+    @IBOutlet var longLabel: UILabel!
+    @IBAction func getLocation() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.requestLocation() //request once
+    //  locationManager.startUpdatingLocation()// continious reques
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let authState = CLLocationManager.authorizationStatus()
+        if authState == .notDetermined{
+            locationManager.requestWhenInUseAuthorization()
+        }
+        locationManager.delegate = self
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error")
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first{
+            latLabel.text = "\(location.coordinate.latitude)"
+            longLabel.text = "\(location.coordinate.longitude)"
+            
+        }
+        
+        
     }
 
 
